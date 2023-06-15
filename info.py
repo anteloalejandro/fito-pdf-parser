9f76cdaee5086d2902cd80baac0d37558d2aaefd
Alejandro Antelo <alfasant@protonmail.com>
2023-06-15 13:51:03 +0200

    to-class

--- info.py
+++ info.py
@@ -1,7 +1,9 @@
+from sys import argv
 import py_pdf_parser as parser
 from py_pdf_parser import loaders
 from py_pdf_parser.visualise import visualise
 
-document = loaders.load_file('./fichas/ES-00981.pdf')
+docname = argv[1] if len(argv) > 1 else './fichas/ES-00981.pdf'
+document = loaders.load_file(docname)
 
 visualise(document, show_info=True)
--- parser.py
+++ parser.py
@@ -4,109 +4,152 @@ from py_pdf_parser import tables
 import csv
 from tabulate import tabulate
 import re
+from py_pdf_parser.visualise import visualise
 
-docname = argv[1] if len(argv) > 1 else './fichas/ES-00981.pdf'
-out_docname = argv[2] if len(argv) > 2 else 'parser.csv'
+class Parser:
 
-FONT_MAPPING = {
-  "Helvetica-Bold,12.0": "header",
-  "Helvetica,9.0": "table_element",
-  "Helvetica-Bold,9.0": "footer_end",
-  "Helvetica-Bold,10.0": "header_start",
-}
-document = load_file(docname, font_mapping=FONT_MAPPING)
+  def __init__(self, pdf_name, csv_name):
+    self.docname = pdf_name
+    self.out_docname = csv_name
 
-headers = document.elements.filter_by_font("header")
+    self.FONT_MAPPING = {
+      "Helvetica-Bold,12.0": "header",
+      "Helvetica,9.0": "table_element",
+      "Helvetica-Bold,9.0": "heading_start",
+      "Helvetica-Bold,10.0": "footer_end",
+    }
+    self.document = load_file(self.docname, font_mapping=self.FONT_MAPPING)
 
-header = headers.filter_by_text_equal(
-  "Usos y Dosis Autorizados"
-).extract_single_element()
+    self.headers = self.document.elements.filter_by_font("header")
 
-post_table = headers.filter_by_text_contains(
-  "Plazos de Seguridad (Protección del Consumidor)"
-).extract_single_element()
+    self.headings = self.document.elements.filter_by_font("heading_start").filter_by_text_equal(
+      "DIRECCIÓN GENERAL DE SANIDAD DE\nLA PRODUCCIÓN AGRARIA"
+    )
 
-elements = document.elements.between(
-  header, post_table
-# ).filter_by_regex(
-#   "^[^P][^á][^g][^i][^n][^a]"
-# ).filter_by_regex(
-#   "[^0-9]"
-# ).filter_by_regex(
-#   "^[^D][^e][^\\s][^0-9]+$"
-).filter_by_font("table_element")
+    self.footers = self.document.elements.filter_by_font("footer_end").filter_by_text_equal(
+      "Página"
+    )
 
-table = tables.extract_table(
-  elements,
-  as_text=True,
-  fix_element_in_multiple_rows=True,
-  fix_element_in_multiple_cols=True,
-)
+    self.header = self.headers.filter_by_text_equal(
+      "Usos y Dosis Autorizados"
+    ).extract_single_element()
 
-def thead():
-  return table[0]
+    self.post_table = self.headers.filter_by_text_contains(
+      "Plazos de Seguridad (Protección del Consumidor)"
+    ).extract_single_element()
 
-def column(n = 0):
-  arr = []
-  for row in table:
-    arr.append(row[n])
+    page = 2
+    heading = self.headings[page]
+    footer = self.footers[page]
+    self.elements = self.document.elements.after(self.header).before(self.post_table).between(
+      heading, footer
+    ).filter_by_font("table_element")
 
-  return arr
+    page += 1
+    while (page < len(self.headings)):
+      heading = self.headings[page]
+      footer = self.footers[page]
+      elements = self.document.elements.after(self.header).before(self.post_table).between(
+        heading, footer
+      ).filter_by_font("table_element")
+      self.elements = self.elements.add_elements(
+        *elements
+      )
+      page += 1
 
-def to_csv(filename = out_docname, table = table):
-  with open(filename, 'w') as f:
-    file = csv.writer(f)
-    file.writerows(table)
+    self.table = tables.extract_table(
+      self.elements,
+      as_text=True,
+      fix_element_in_multiple_rows=True,
+      fix_element_in_multiple_cols=True,
+      tolerance=1000
+    )
 
-def single_page_format():
-  arr = []
-  for row in table:
-    arr.append([row[0], row[1], row[2]])
+  def thead(self):
+    return self.table[0]
 
-  return arr
+  def column(self, n = 0):
+    arr = []
+    for row in self.table:
+      arr.append(row[n])
+
+    return arr
+
+  def to_csv(self, filename = None, table = None):
+    if filename == None: filename = self.out_docname
+    if table == None: table = self.table
+
+    with open(filename, 'w') as f:
+      file = csv.writer(f)
+      file.writerows(table)
+
+  def single_page_format(self):
+    arr = []
+    for row in self.table:
+      arr.append([row[0], row[1], row[2]])
+
+    return arr
 
 # Actualmente 'single_page_format' sirve para todas las tablas.
 # Con el nuevo formato directamente no funciona
-def multi_page_format():
-  arr = []
-  for row in table:
-    por_filtrar = row[6]
+  def multi_page_format(self):
+    arr = []
+    for row in self.table:
+      por_filtrar = row[6]
 
-    if (re.search('^Ámbito', por_filtrar)):
-      por_filtrar = ''
+      if (re.search('^Ámbito', por_filtrar)):
+        por_filtrar = ''
 
-    usos = row[0] + row[1] + row[2]
-    agentes = row[3]
-    dosis = por_filtrar + row[7]
+      usos = row[0] + row[1] + row[2]
+      agentes = row[3]
+      dosis = por_filtrar + row[7]
 
-    if (re.search('Página', usos)):
-      usos = ''
-    arr.append([usos, agentes, dosis])
+      if (re.search('Página', usos)):
+        usos = ''
+      arr.append([usos, agentes, dosis])
 
-  multi_page_fix(arr)
-  return arr
+    self.multi_page_fix(arr)
+    return arr
 
-def multi_page_fix(table):
-  arr = []
-  for i in range(0, len(table)):
-    row = table[i]
-    if not row[1]:
-      last_row = table[i-1]
-      print('---MATCH----')
-      print('---BEFORE---')
-      print(last_row)
-      print('---ROW------')
-      print(row)
+  def multi_page_fix(self, table):
+    arr = []
+    for i in range(0, len(table)):
+      row = table[i]
+      if not row[1]:
+        last_row = table[i-1]
+        print('---MATCH----')
+        print('---BEFORE---')
+        print(last_row)
+        print('---ROW------')
+        print(row)
 
-  return arr
+    return arr
 
-def format():
-  # if thead()[0] == '':
-  #   return multi_page_format()
-  return single_page_format()
+  def format(self):
+    # if thead()[0] == '':
+    #   return multi_page_format()
+    return self.single_page_format()
+
+  def get_raw_text(self):
+    arr = []
+    for e in self.elements:
+      arr.append(e.text())
+    return arr
+
+  def list_elements(self):
+    element_list = self.get_raw_text()
+    for i in range(0, len(element_list)):
+      print(i+1, element_list[i], '\n')
 
 # formated_table = format()
 # to_csv(table=formated_table)
 # print(tabulate(formated_table))
 
-print(to_csv())
+# print(to_csv())
+
+docname = argv[1] if len(argv) > 1 else './fichas/ES-00318.pdf'
+out_docname = argv[2] if len(argv) > 2 else 'parser.csv'
+
+parser = Parser(docname, out_docname)
+
+print(tabulate(parser.format()))
